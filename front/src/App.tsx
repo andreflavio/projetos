5e5f312244f3fbccdae80e2f403a58952c94d854
SamuelLucasVieira <samuellucasvieira.melo@gmail.com>
2024-04-17 16:49:04 -0300

    Exclusão da função login do arquivo App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,5 @@
 // import { Provider } from "./contexts";
 // import Principal from "./pages/Principal";
-
-import Login from "./componets/Login";
 import AppRoutes from "./routes";
 
 export default function App() {
